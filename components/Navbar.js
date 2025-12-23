ba60174e2708f0ca2177dbd56dc96d1c30290afa
Shreyas <shreyasbandekar01@gmail.com>
2025-12-23 20:59:18 +0530

    refactor: adjust Navbar component height and logo styling

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='h-20 bg-violet-400 flex justify-between px-7 items-center'>
-      <div className='logo font-bold mr-8 font-mono text-2xl text-amber-50'>
+    <nav className='h-16 bg-violet-400 flex justify-between px-7 items-center'>
+      <div className='logo font-extrabold font-mono text-xl text-amber-50'>
         BlinkURL
       </div>
-      <ul className='flex justify-center gap-4 items-center text-amber-50 font-semibold'>
+      <ul className='flex justify-center gap-6 items-center text-amber-50 font-semibold'>
         <li>Home</li>
         <li>About</li>
         <li>Shorten</li>
