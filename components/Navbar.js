1650bfc282e4efe65ee5d11d85cb17e40f0c67ba
Shreyas <shreyasbandekar01@gmail.com>
2025-12-23 20:45:51 +0530

    refactor: enhance Navbar component styling and update menu items

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,11 +2,12 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav>
-      <ul>
+    <nav className='h-8 bg-cyan-400 flex justify-center items-center'>
+      <ul className='flex justify-center gap-4 items-center'>
         <li>Home</li>
         <li>About</li>
-        <li>Contact</li>
+        <li>Shorten</li>
+        <li>Contact Us</li>
       </ul>
     </nav>
   )
