eb2bd523b87f8d4e6deacc614c708331fea7fe1d
Shreyas <shreyasbandekar01@gmail.com>
2025-12-23 20:51:19 +0530

    refactor: update Navbar component layout and styling

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='h-8 bg-cyan-400 flex justify-center items-center'>
-      <ul className='flex justify-center gap-4 items-center'>
+    <nav className='h-20 bg-violet-400 flex justify-between px-7 items-center'>
+      <div className='logo font-bold mr-8 font-mono text-2xl text-amber-50'>
+        BlinkURL
+      </div>
+      <ul className='flex justify-center gap-4 items-center text-amber-50 font-semibold'>
         <li>Home</li>
         <li>About</li>
         <li>Shorten</li>
