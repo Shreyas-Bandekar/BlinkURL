3fcb56d680695bce4ad33c93153fbeec674429b1
Shreyas <shreyasbandekar01@gmail.com>
2026-01-01 12:57:44 +0530

    Refactor code structure for improved readability and maintainability

--- app/page.js
+++ app/page.js
@@ -1,8 +1,17 @@
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <main>  
-      <h1>Welcome to BlinkURL</h1>  
+      <section className="bg-blue-200">
+        <div className="p-4">
+          <p>The best URL Shortener</p>
+          <p>Shorten your long URLs quickly and easily.</p>
+        </div>
+        <div className="bg-amber-200">
+          <Image src={"/vector.jpg"} alt="URL Shortener" width={600} height={400} />
+        </div>
+      </section> 
     </main> 
   );
 }
