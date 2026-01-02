500069ba7d496ecbb10cd84eef26e9991b741aa0
Shreyas <shreyasbandekar01@gmail.com>
2026-01-02 20:03:52 +0530

    refactor: update layout structure in Home component for improved responsiveness

--- app/page.js
+++ app/page.js
@@ -3,13 +3,13 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main>  
-      <section className="bg-blue-200">
-        <div className="p-4">
+      <section className="grid grid-cols-2 h-[50vh]">
+        <div className="bg-blue-200">
           <p>The best URL Shortener</p>
           <p>Shorten your long URLs quickly and easily.</p>
         </div>
-        <div className="bg-amber-200">
-          <Image src={"/vector.jpg"} alt="URL Shortener" width={600} height={400} />
+        <div className="bg-amber-200 flex justify-start relative">
+          <Image src={"/vector.jpg"} alt="URL Shortener" fill={true} />
         </div>
       </section> 
     </main> 
