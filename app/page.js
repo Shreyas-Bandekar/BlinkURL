912e14e0de3fe86a744bf9d3c9c0198c554247be
Shreyas <shreyasbandekar01@gmail.com>
2026-01-02 20:57:37 +0530

    refactor: update font size and apply custom font for improved typography

--- app/page.js
+++ app/page.js
@@ -1,11 +1,17 @@
 import Image from 'next/image';
+import localFont from 'next/font/local';
+
+const poppins = localFont({
+  src: 'fonts/Poppins-ExtraBold.ttf',
+  variable: '--font-poppins',
+});
 
 export default function Home() {
   return (
     <main className='bg-cyan-100'>  
       <section className="grid grid-cols-2 h-[50vh]">
         <div className="flex flex-col gap-4 items-center justify-center">
-          <p className='text-2xl font-bold '>The best URL Shortener</p>
+          <p className={`text-4xl font-bold ${poppins.className}`}>The best URL Shortener</p>
           <p>Shorten your long URLs quickly and easily.</p>
         </div>
         <div className="flex justify-start relative">
